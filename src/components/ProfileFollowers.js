373580adeb4bb01139ee0b9d46beae80e7574938
roy <89403308+itsaruproy@users.noreply.github.com>
2022-10-25 20:15:20 +0530

    feat: add followers and followings

--- src/components/ProfileFollowers.js
+++ src/components/ProfileFollowers.js
@@ -1,6 +1,22 @@
+import { Box, HStack, Avatar, Text, VStack } from '@chakra-ui/react'
+import { AiOutlineUser } from 'react-icons/ai'
 import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { fetchProfileFollowers, tabChange } from '../actions'
+
+const Follower = ({ username }) => {
+    return (
+        <Link to={`/profile/${username}`}>
+            <Box shadow={'md'} px={'1rem'} py={'0.5rem'} width={'350px'}>
+                <HStack justifyContent={'space-between'}>
+                    <Avatar icon={<AiOutlineUser />} />
+                    <Text fontWeight={'semibold'}>{username}</Text>
+                </HStack>
+            </Box>
+        </Link>
+    )
+}
 
 const ProfileFollowers = props => {
     const username = props.match.params.username
@@ -14,11 +30,11 @@ const ProfileFollowers = props => {
         fetchProfileFollowers(username)
     }, [fetchProfileFollowers, username])
     return (
-        <>
-            {props.ProfileFollowers.map(post => {
-                return <div>This is a follower</div>
+        <VStack gap={'2'} mt={'1'}>
+            {props.ProfileFollowers.map((follower, index) => {
+                return <Follower key={index} username={follower.username} />
             })}
-        </>
+        </VStack>
     )
 }
 
--- src/components/ProfileFollowing.js
+++ src/components/ProfileFollowing.js
@@ -1,6 +1,22 @@
+import { Box, HStack, Avatar, Text, VStack } from '@chakra-ui/react'
+import { AiOutlineUser } from 'react-icons/ai'
 import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { fetchProfileFollowings, tabChange } from '../actions'
+
+const Following = ({ username }) => {
+    return (
+        <Link to={`/profile/${username}`}>
+            <Box shadow={'md'} px={'1rem'} py={'0.5rem'} width={'350px'}>
+                <HStack justifyContent={'space-between'}>
+                    <Avatar icon={<AiOutlineUser />} />
+                    <Text fontWeight={'semibold'}>{username}</Text>
+                </HStack>
+            </Box>
+        </Link>
+    )
+}
 
 const ProfileFollowing = props => {
     const username = props.match.params.username
@@ -14,11 +30,11 @@ const ProfileFollowing = props => {
         fetchProfileFollowings(username)
     }, [fetchProfileFollowings, username])
     return (
-        <>
-            {props.ProfileFollowings.map(post => {
-                return <div>This is a following</div>
+        <VStack gap={'2'} mt={'1'}>
+            {props.ProfileFollowings.map((following, index) => {
+                return <Following key={index} username={following.username} />
             })}
-        </>
+        </VStack>
     )
 }
 
