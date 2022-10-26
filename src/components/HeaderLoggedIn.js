2f78d7f9b18b9c304025f49f944cc53c4fffd4d5
roy <89403308+itsaruproy@users.noreply.github.com>
2022-10-26 23:27:00 +0530

    feat: Add search bar to find posts

--- src/components/HeaderLoggedIn.js
+++ src/components/HeaderLoggedIn.js
@@ -1,23 +1,104 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {
     Button,
     HStack,
     Icon,
     Link as ChakraLink,
     Avatar,
+    useDisclosure,
+    Flex,
+    Input,
+    Text,
+} from '@chakra-ui/react'
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
 } from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
+import { SearchIcon } from '@chakra-ui/icons'
 import { AiOutlineUser } from 'react-icons/ai'
 
 import { connect } from 'react-redux'
 import { signOut } from '../actions'
+import { BASE_URL } from '../constants'
+import axios from 'axios'
+
+function SearchModal(props) {
+    const { isOpen, onOpen, onClose } = props
+    const [posts, setPosts] = useState([])
+    const [term, setTerm] = useState('')
+    const [finalterm, setFinalterm] = useState()
+
+    useEffect(() => {
+        const termid = setTimeout(() => {
+            setFinalterm(term)
+        }, 1000)
+
+        return () => clearTimeout(termid)
+    }, [term])
+
+    useEffect(() => {
+        if (finalterm) {
+            async function fetchResults() {
+                try {
+                    const response = await axios.post(BASE_URL + '/search', {
+                        searchTerm: finalterm,
+                    })
+                    setPosts(response.data)
+                } catch (e) {
+                    console.log('prolem')
+                }
+            }
+            fetchResults()
+        }
+    }, [finalterm])
+
+    return (
+        <>
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <HStack
+                        py={'1rem'}
+                        px={'1rem'}
+                        spacing={'5px'}
+                        justifyContent={'space-between'}
+                    >
+                        <Input
+                            value={term}
+                            onChange={e => setTerm(e.target.value)}
+                            placeholder={'Search articles'}
+                        />
+                    </HStack>
+                    <ModalCloseButton />
+
+                    <ModalBody>
+                        <Flex direction={'column'}>
+                            {posts.map(post => {
+                                return <Text>{post.title}</Text>
+                            })}
+                        </Flex>
+                    </ModalBody>
+                </ModalContent>
+            </Modal>
+        </>
+    )
+}
 
 const HeaderLoggedIn = props => {
+    const { isOpen, onOpen, onClose } = useDisclosure()
     const signOutHandler = () => {
         props.signOut()
     }
     return (
         <HStack spacing={'2'} alignItems={'center'}>
+            <SearchIcon onClick={onOpen} />
+            <SearchModal isOpen={isOpen} onOpe={onOpen} onClose={onClose} />
             <ChakraLink as={Link} to={`/profile/${props.Username}`}>
                 <Avatar w={'8'} h={'8'} icon={<AiOutlineUser />} />
             </ChakraLink>
