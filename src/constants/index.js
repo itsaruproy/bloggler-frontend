a1d64316ad2311fe5b754fcb2b6e470ee0123da5
roy <89403308+itsaruproy@users.noreply.github.com>
2022-10-28 12:18:01 +0530

    fix: removed aunnecessary constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,1 @@
 export const BASE_URL = 'http://localhost:8000'
-export const SCOPES = 'https://www.googleapis.com/auth/drive'
-export const CLIENT_ID =
-    '456086651360-r6v9clrue5ri8jhrlpm5it8okh2nttl5.apps.googleusercontent.com'
